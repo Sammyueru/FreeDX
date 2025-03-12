f6fcf980d0df518126f7087a9d3a990fb364b494
Sammyueru <samm@sammtech.net>
2025-03-12 10:38:11 -0600

    added main driver

---
+++ src/freedx/main.c
@@ -0,0 +1,14 @@
+/* FreeDX - Copyright (c) 2025 Samm, Licensed under MIT License. */
+
+int main(void) {
+    /* 0. CPU
+     * 1. GPU */
+    uint8_t compute_mode = 0;
+    
+    int16_t exit = 0;
+    while (!exit) {
+        
+    }
+    return 0;
+}
+
